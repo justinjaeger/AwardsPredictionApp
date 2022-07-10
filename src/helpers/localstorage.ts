553754d6dd4a147976266b40beab794c5feec804
Justin Jaeger <jjustinjaeger@gmail.com>
2022-07-10 14:30:45 -0400

    nit

--- src/helpers/localstorage.ts
+++ src/helpers/localstorage.ts
@@ -1,5 +1,10 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+/**
+ * Unused for now, but might be handy later if wanting to do any caching
+ * or store user preferences
+ */
+
 const _storeData = async (key: string, value: string) => {
   try {
     return await AsyncStorage.setItem(key, value);
