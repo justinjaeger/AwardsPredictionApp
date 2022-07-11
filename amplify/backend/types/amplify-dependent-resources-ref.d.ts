d7c02d2251df121b888d9f652593859d5ce71725
Justin Jaeger <jjustinjaeger@gmail.com>
2022-07-10 23:50:05 -0400

    feat: setup amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "predictionsapp3d2f3c1f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import { AppRegistry } from 'react-native';
 import App from './src/app';
 import { name as appName } from './app.json';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+
+/**
+ * withAuthenticator only lets signed-in users access the app
+ * routing for login pages and giving access to App is managed automatically
+ */
+
+Amplify.configure(awsconfig);
 
 AppRegistry.registerComponent(appName, () => App);
