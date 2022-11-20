395d7bff7b46dcb40039b3828a53e072187a2658
Justin Jaeger <jjustinjaeger@gmail.com>
2022-11-20 14:14:30 -0500

    dark background by default

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,30 +1,22 @@
 import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // import RNBootSplash from 'react-native-bootsplash'; // splash screen (https://github.com/zoontek/react-native-bootsplash)
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './MainNavigator';
-import { useAppDispatch } from '../store';
-import { thunkGetAllEvents } from '../store/thunks/getAllEventsAndCategories';
-import { View } from 'react-native';
 
 // onReady={() => RNBootSplash.hide()} (could add to NavigationContainer)
 const Navigation = () => {
-  //   const [loading, setLoading] = useState<boolean>(true);
-
-  //   const dispatch = useAppDispatch();
-
-  //   useEffect(() => {
-  //     dispatch(thunkGetAllEvents())
-  //       .then((res) => {
-  //         console.error('res', res);
-  //       })
-  //       .catch((err) => {
-  //         console.error('err', err);
-  //       });
-  //   }, []);
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: 'rgba(0,0,0,0.6)',
+      border: 'transparent',
+    },
+  };
 
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={theme}>
       <MainNavigator />
     </NavigationContainer>
   );
