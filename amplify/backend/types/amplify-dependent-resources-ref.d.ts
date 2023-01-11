16fa9b382e76f4c7ff59fe961e7ca5a1b39e86f4
Justin Jaeger <jjustinjaeger@gmail.com>
2023-01-11 16:56:39 -0500

    recreate amplify storage with no luck

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,49 +1,49 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "awardsappa4e66206": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "api": {
-        "awardsapp": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "function": {
-        "updateCommunityPredictions": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string",
-            "CloudWatchEventRule": "string"
-        },
-        "updatePersonalHistory": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string",
-            "CloudWatchEventRule": "string"
-        },
-        "updateCommunityHistory": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string",
-            "CloudWatchEventRule": "string"
-        }
-    },
-    "storage": {
-        "awardsappimages": {
-            "BucketName": "string",
-            "Region": "string"
-        }
-    }
-}
+  auth: {
+    awardsappa4e66206: {
+      IdentityPoolId: 'string';
+      IdentityPoolName: 'string';
+      UserPoolId: 'string';
+      UserPoolArn: 'string';
+      UserPoolName: 'string';
+      AppClientIDWeb: 'string';
+      AppClientID: 'string';
+    };
+  };
+  api: {
+    awardsapp: {
+      GraphQLAPIKeyOutput: 'string';
+      GraphQLAPIIdOutput: 'string';
+      GraphQLAPIEndpointOutput: 'string';
+    };
+  };
+  function: {
+    updateCommunityPredictions: {
+      Name: 'string';
+      Arn: 'string';
+      Region: 'string';
+      LambdaExecutionRole: 'string';
+      CloudWatchEventRule: 'string';
+    };
+    updatePersonalHistory: {
+      Name: 'string';
+      Arn: 'string';
+      Region: 'string';
+      LambdaExecutionRole: 'string';
+      CloudWatchEventRule: 'string';
+    };
+    updateCommunityHistory: {
+      Name: 'string';
+      Arn: 'string';
+      Region: 'string';
+      LambdaExecutionRole: 'string';
+      CloudWatchEventRule: 'string';
+    };
+  };
+  storage: {
+    awardsappstorage: {
+      BucketName: 'string';
+      Region: 'string';
+    };
+  };
+};
