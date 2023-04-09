a0fde9f2055f4a907985435ef81b9d3ad11dc1e4
Justin Jaeger <jjustinjaeger@gmail.com>
2023-04-09 18:59:57 -0400

    update icon and trophy pngs

--- src/components/AwardsBodyImage/index.tsx
+++ src/components/AwardsBodyImage/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { AwardsBody } from '../../API';
 import useDevice from '../../util/device';
-import Trophy from '../../assets/awardsBodies/trophy.svg';
-import TrophyWhite from '../../assets/awardsBodies/trophy_white.svg';
+import Trophy from '../../assets/awardsBodies/trophy_color.png';
+import TrophyWhite from '../../assets/awardsBodies/trophy.png';
 
 const DEFAULT_SIZE = 60;
 
@@ -12,29 +13,21 @@ const AwardsBodyImage = (props: {
   size?: number;
   style?: any;
 }) => {
-  const { white, size } = props;
+  const { white, size, style } = props;
   const { isPad } = useDevice();
 
   const _size = (size || DEFAULT_SIZE) * (isPad ? 1.5 : 1);
 
-  return white ? (
-    <TrophyWhite width={_size} height={_size} />
-  ) : (
-    <Trophy width={_size} height={_size} />
+  const source = white ? TrophyWhite : Trophy;
+
+  return (
+    <Image
+      source={source}
+      width={_size}
+      height={_size}
+      style={{ width: _size, height: _size, ...style }}
+    />
   );
-
-  //   const source = white
-  //     ? AWARDS_BODY_TO_IMAGE_WHITE[awardsBody]
-  //     : AWARDS_BODY_TO_IMAGE[awardsBody];
-
-  //   return (
-  //     <Image
-  //       source={source}
-  //       width={_size}
-  //       height={_size}
-  //       style={{ width: _size, height: _size, ...style }}
-  //     />
-  //   );
 };
 
 export default AwardsBodyImage;
