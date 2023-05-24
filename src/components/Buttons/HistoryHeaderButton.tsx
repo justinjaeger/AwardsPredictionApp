ee17bdd228e400e54c1686a20a6bb73bc3695bf8
Justin Jaeger <jjustinjaeger@gmail.com>
2023-05-24 01:12:51 -0400

    chore: disable history tab

--- src/components/Buttons/HistoryHeaderButton.tsx
+++ src/components/Buttons/HistoryHeaderButton.tsx
@@ -1,51 +1,52 @@
-import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { EventStatus } from '../../API';
-import theme from '../../constants/theme';
-import { useCategory } from '../../context/CategoryContext';
-import { iEvent } from '../../types';
-import Snackbar from '../Snackbar';
-import { IconButton } from './IconButton';
+// import { useNavigation } from '@react-navigation/native';
+// import React from 'react';
+// import { EventStatus } from '../../API';
+// import theme from '../../constants/theme';
+// import { useCategory } from '../../context/CategoryContext';
+// import { iEvent } from '../../types';
+// import Snackbar from '../Snackbar';
+// import { IconButton } from './IconButton';
 
 const HistoryHeaderButton = ({ isDisabled }: { isDisabled?: boolean }) => {
-  const navigation = useNavigation();
-  const { date, setDate, event: _event } = useCategory();
-  const isHistory = !!date;
-  const event = _event as iEvent;
-  const isArchived = event.status === EventStatus.ARCHIVED;
+  return null;
+  //   const navigation = useNavigation();
+  //   const { date, setDate, event: _event } = useCategory();
+  //   const isHistory = !!date;
+  //   const event = _event as iEvent;
+  //   const isArchived = event.status === EventStatus.ARCHIVED;
 
-  const onPress = () => {
-    if (isArchived) {
-      Snackbar.warning('Time Machine is always on when event has ended');
-      return;
-    }
-    if (isDisabled) {
-      Snackbar.warning('Time Machine disabled while editing predictions');
-      return;
-    }
-    if (!isHistory) {
-      setDate(new Date());
-    } else {
-      setDate(undefined);
-    }
-  };
+  //   const onPress = () => {
+  //     if (isArchived) {
+  //       Snackbar.warning('Time Machine is always on when event has ended');
+  //       return;
+  //     }
+  //     if (isDisabled) {
+  //       Snackbar.warning('Time Machine disabled while editing predictions');
+  //       return;
+  //     }
+  //     if (!isHistory) {
+  //       setDate(new Date());
+  //     } else {
+  //       setDate(undefined);
+  //     }
+  //   };
 
-  // hide if is archived
-  if (isDisabled) return null;
+  //   // hide if is archived
+  //   if (isDisabled) return null;
 
-  return (
-    <IconButton
-      iconProps={{ name: 'clock-outline' }}
-      onPress={() => {
-        if (onPress) {
-          onPress();
-        } else {
-          navigation.goBack();
-        }
-      }}
-      styles={{ marginRight: theme.windowMargin }}
-    />
-  );
+  //   return (
+  //     <IconButton
+  //       iconProps={{ name: 'clock-outline' }}
+  //       onPress={() => {
+  //         if (onPress) {
+  //           onPress();
+  //         } else {
+  //           navigation.goBack();
+  //         }
+  //       }}
+  //       styles={{ marginRight: theme.windowMargin }}
+  //     />
+  //   );
 };
 
 export default HistoryHeaderButton;
