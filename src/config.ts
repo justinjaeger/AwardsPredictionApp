a48e3868a21f2c6f652b065551758b9c1f1a1292
Justin Jaeger <jjustinjaeger@gmail.com>
2023-07-01 22:41:57 -0400

    fix: replace env config package

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,10 @@
-import { Config as RNConfig } from 'react-native-config';
+import {
+  TMDB_API_KEY as ENV_TMDB_API_KEY,
+  SLACK_BOT_TOKEN as ENV_SLACK_BOT_TOKEN,
+  JWT_SECRET as ENV_JWT_SECRET,
+  // eslint-disable-next-line import/no-unresolved
+} from '@env';
 
-export const TMDB_API_KEY = RNConfig.TMDB_API_KEY as string;
-export const SLACK_BOT_TOKEN = RNConfig.SLACK_BOT_TOKEN as string;
-export const JWT_SECRET = RNConfig.JWT_SECRET as string;
+export const TMDB_API_KEY = ENV_TMDB_API_KEY as string;
+export const SLACK_BOT_TOKEN = ENV_SLACK_BOT_TOKEN as string;
+export const JWT_SECRET = ENV_JWT_SECRET as string;
---
+++ src/types/env.d.tsx
@@ -0,0 +1,6 @@
+declare module '@env' {
+  export const TMDB_API_KEY: string;
+  export const SLACK_BOT_TOKEN: string;
+  export const JWT_SECRET: string;
+  export const SECRET_JWT: string;
+}
