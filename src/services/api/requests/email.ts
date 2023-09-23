ad96ab745dadd90b6be6ead1e27cf04548376073
Justin Jaeger <jjustinjaeger@gmail.com>
2023-09-23 17:32:18 -0400

    fix: change name of email requests; verify magic link returns a string

--- src/services/api/requests/email.ts
+++ src/services/api/requests/email.ts
@@ -1,9 +1,10 @@
 import api from '../api';
 
-export const sendEmail = async ({ email }: { email: string }) => {
+export const sendVerificationEmail = async (email: string) => {
   return await api.get<undefined>(`email/send/?email=${email}`);
 };
 
-export const verifyEmail = async ({ link }: { link: string }) => {
-  return await api.get<undefined>(`email/verify/?link=${link}`);
+// returns the email parsed from the link
+export const verifyEmailMagicLink = async (link: string) => {
+  return await api.get<string>(`email/verify/?link=${link}`);
 };
