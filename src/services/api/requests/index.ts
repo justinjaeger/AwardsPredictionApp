e4e46cd6ec60e53d8e24d184e9f1ef82fc2ee2c8
Justin Jaeger <jjustinjaeger@gmail.com>
2023-09-23 00:35:36 -0400

    feat: relationship requests

--- src/services/api/requests/index.ts
+++ src/services/api/requests/index.ts
@@ -3,6 +3,7 @@ import * as TokenRequests from './token';
 import * as SongRequests from './song';
 import * as MovieRequests from './movie';
 import * as PersonRequests from './person';
+import * as RelationshipRequests from './relationship';
 
 const MongoApi = {
   ...UserRequests,
@@ -10,6 +11,7 @@ const MongoApi = {
   ...SongRequests,
   ...MovieRequests,
   ...PersonRequests,
+  ...RelationshipRequests,
 };
 
 export default MongoApi;
