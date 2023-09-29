70fa2b70b4f32d34076bbcc4e149c5911db7d139
Justin Jaeger <jjustinjaeger@gmail.com>
2023-09-29 01:23:16 -0400

    feat: add helper func getPhaseUserIsPredicting

---
+++ src/util/getPhaseUserIsPredicting.ts
@@ -0,0 +1,22 @@
+import { EventModel, EventStatus, Phase } from '../types/api';
+
+export const getPhaseUserIsPredicting = (
+  event: EventModel,
+  shortlistDateTime: Date | undefined,
+) => {
+  const { status, nomDateTime, winDateTime } = event;
+  const shortlistDateHasPassed = !!(shortlistDateTime && shortlistDateTime < new Date());
+  const nomDateHasPassed = !!(nomDateTime && nomDateTime < new Date());
+  const winDateHasPassed = !!(winDateTime && winDateTime < new Date());
+  const canPredictWinners = !winDateHasPassed && status === EventStatus.WINS_LIVE;
+  const canPredictNominations = !nomDateHasPassed && status === EventStatus.NOMS_LIVE;
+  const canPredictShortlist = !shortlistDateHasPassed && status === EventStatus.NOMS_LIVE;
+  const phaseUserIsPredicting = canPredictWinners
+    ? Phase.WINNER
+    : canPredictShortlist
+    ? Phase.SHORTLIST
+    : canPredictNominations
+    ? Phase.NOMINATION
+    : Phase.CLOSED;
+  return phaseUserIsPredicting;
+};
