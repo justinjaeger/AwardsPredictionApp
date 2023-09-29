2505d23e43c9a76b3bee04eb4ba689e03def451c
Justin Jaeger <jjustinjaeger@gmail.com>
2023-09-29 01:13:09 -0400

    fix: add isOpen param to event request

--- src/services/api/requests/event.ts
+++ src/services/api/requests/event.ts
@@ -2,21 +2,37 @@ import { EventModel, WithId } from '../../../types/api';
 import api from '../api';
 
 export const getEvents = async ({
-  yearOfEvent,
+  maxYear,
+  minYear,
   awardsBody,
+  isOpen,
 }: {
-  yearOfEvent?: number;
+  maxYear?: number;
+  minYear?: number;
   awardsBody?: string;
+  isOpen?: boolean;
 }) => {
   let queryString = '?';
-  if (yearOfEvent) {
-    queryString += `maxYear=${yearOfEvent}&minYear=${yearOfEvent}`;
+  if (maxYear) {
+    queryString += `maxYear=${maxYear}`;
+  }
+  if (minYear) {
+    if (queryString !== '?') {
+      queryString += '&';
+    }
+    queryString += `minYear=${minYear}`;
   }
   if (awardsBody) {
     if (queryString !== '?') {
       queryString += '&';
     }
     queryString += `awardsBody=${awardsBody}`;
+  }
+  if (isOpen) {
+    if (queryString !== '?') {
+      queryString += '&';
+    }
+    queryString += `isOpen=${isOpen}`;
   }
 
   return await api.get<WithId<EventModel>[]>(
