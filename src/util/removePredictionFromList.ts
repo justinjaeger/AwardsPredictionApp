4a6a917f9da923f48b7b772cf9106cd2654bd14f
Justin Jaeger <jjustinjaeger@gmail.com>
2023-10-02 03:32:12 -0400

    fix: broken type check

--- src/util/removePredictionFromList.ts
+++ src/util/removePredictionFromList.ts
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { iPrediction } from '../types/keys';
+import { iPrediction } from '../types/api';
 
 export const removePredictionFromList = (
   arr: iPrediction[],
