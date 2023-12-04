96077f2e751e344f030abb663d82fd7b6625ff7b
Justin Jaeger <jjustinjaeger@gmail.com>
2023-12-04 01:36:40 -0500

    fix: format for tablets

--- src/util/device.tsx
+++ src/util/device.tsx
@@ -2,12 +2,13 @@ import { Platform, useWindowDimensions } from 'react-native';
 
 const useDevice = () => {
   const { width } = useWindowDimensions();
-  // @ts-ignore
-  const isPad = Platform.constants?.interfaceIdiom === 'pad'; // boolean
-  const isAndroid = Platform.OS === 'android';
 
   const isSmallScreen = width < 400;
-  const isLargeScreen = width > 900;
+  const isLargeScreen = width > 700;
+
+  // @ts-ignore
+  const isPad = Platform.constants?.interfaceIdiom === 'pad' || isLargeScreen; // boolean
+  const isAndroid = Platform.OS === 'android';
 
   return { isPad, isAndroid, isSmallScreen, isLargeScreen };
 };
