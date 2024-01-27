5899fcf83fed9f296f2d854491c5a42a8e463f34
Justin Jaeger <jjustinjaeger@gmail.com>
2024-01-26 21:04:08 -0500

    feat: hide events with isHidden property from  non admin users

--- src/hooks/queries/useQueryGetAllEvents.tsx
+++ src/hooks/queries/useQueryGetAllEvents.tsx
@@ -1,13 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
 import { QueryKeys } from '../../types/keys';
 import MongoApi from '../../services/api/requests';
+import { useAuth } from '../../context/AuthContext';
+import { UserRole } from '../../models';
 
 const useQueryGetAllEvents = () => {
+  const { userRole } = useAuth();
   const { isLoading, data, refetch } = useQuery({
     queryKey: [QueryKeys.EVENTS],
     queryFn: async () => {
       const { data: events } = await MongoApi.getEvents({});
-      return events || [];
+      const filteredEvents = (events ?? []).filter((event) => {
+        if (event.isHidden) {
+          return userRole === UserRole.ADMIN;
+        }
+        return true;
+      });
+      return filteredEvents;
     },
   });
 
