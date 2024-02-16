ba252edc6c47adad84b23576b6055906164fd87a
Justin Jaeger <jjustinjaeger@gmail.com>
2024-02-15 20:23:37 -0500

    chore: remove liveAt field on Event model

--- src/models.ts
+++ src/models.ts
@@ -161,7 +161,6 @@ export type EventModel = {
   year: number;
   status: EventStatus;
   accoladeId?: ObjectId;
-  liveAt?: Date;
   shortlistDateTime?: Date;
   nomDateTime?: Date;
   winDateTime?: Date;
