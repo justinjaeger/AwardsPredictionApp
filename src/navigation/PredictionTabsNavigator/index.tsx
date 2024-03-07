8a73b43809543b5d8ed09bdb3fc6a7832e8a1a28
Justin Jaeger <jjustinjaeger@gmail.com>
2024-03-07 00:06:02 -0500

    chore: delete outdated comment

--- src/navigation/PredictionTabsNavigator/index.tsx
+++ src/navigation/PredictionTabsNavigator/index.tsx
@@ -4,10 +4,6 @@ import { useAuth } from '../../context/AuthContext';
 import { useRouteParams } from '../../hooks/useRouteParams';
 import SectionTopTabs from '../../components/SectionTopTabs';
 
-/**
- * Note: This component is a bit whack but it's done in this way to prevent re-renders of lists
- * We call the setPersonalCommunityTab function manually WHEN we navigate, instead of calling it inside here
- */
 const PredictionTabsNavigator = ({
   onChangeTab,
   initialTabIndex = 0,
